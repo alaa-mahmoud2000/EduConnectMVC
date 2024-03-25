3e2d0330460610127b489ae56dbec108ef8f7959
Amira Abdelhafeez <aabdelhafeez@solugrowth.com>
2024-03-25 20:18:22 +0200

    Add project files.

---
+++ EduConnect/Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EduConnect.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base (options)
+        {
+            
+        }
+    }
+}
---
+++ EduConnect/Models/University.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EduConnect.Models
+{
+    public class University
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Description { get; set; }
+    }
+}
