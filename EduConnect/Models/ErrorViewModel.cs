dde2805ef22ddae1132766aafa224ae9c8c9bd41
Amira Abdelhafeez <aabdelhafeez@solugrowth.com>
2024-06-07 15:11:11 +0300

    Api + front end application (Alaa)
    Added API Authentication with token
    Added front end web application.
    Added UI(intergration) to the component and divided styles
    Added guards
    added services for web api calls and user service to manage userdata on the browser.

--- EduConnect/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace EduConnect.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
