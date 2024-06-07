49c423b7ce7367964d5a193eb7f2151c882183ba
Amira Abdelhafeez <aabdelhafeez@solugrowth.com>
2024-06-08 02:36:08 +0300

    Colleges (list)

--- EduConnectApp/EduConnect/src/app/student/colleges/colleges.component.ts
+++ EduConnectApp/EduConnect/src/app/student/colleges/colleges.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { EduApiService } from '../../core/services/edu-api.service';
+import { ICollege } from '../../shared/interfaces';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-colleges',
@@ -6,5 +9,18 @@ import { Component } from '@angular/core';
   styleUrl: './colleges.component.css'
 })
 export class CollegesComponent {
+  colleges : ICollege[] = [];
+  constructor(private apiService : EduApiService , private toastr : ToastrService){
 
+  }
+  ngOnInit(){
+    this.apiService.getColleges().subscribe({
+      next: (response) => {
+        this.colleges = response;
+      },
+      error: (error) => {
+        this.toastr.error('Error' + error.error , 'Error');
+      }
+    });
+  }
 }
